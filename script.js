bafe2d3c0d04d176066f1952b0f997765f0b0b30
sanishmdhr96 <mail.sanishmanandhar@gmail.com>
2024-06-04 19:19:29 +0545

    add items with objects and array logics

--- script.js
+++ script.js
@@ -1,18 +1,46 @@
 // Button click -> input field ko data tanna paryo
 
+const todoList = []
+
 function addTodoItem() {
     // get input value
     const inputElement = document.getElementById('new-todo-item')
     const inputElementValue = inputElement.value
 
+    // TODO list object
+    const newTodoItem = {
+        text: inputElementValue,
+        completed: false
+    }
+
+    todoList.push(newTodoItem)
+
+    updateTodoList()
+
+    inputElement.value = ''
+
+}
+
+function updateTodoList() {
     // Get the todo element
     const todoListElement = document.getElementById('todo-list')
 
-    // create list item
-    const listItem = document.createElement('li')
-    listItem.textContent = inputElementValue
+    // clear the existing list items
+    todoListElement.innerHTML = ''
 
-    // add item to list
-    todoListElement.appendChild(listItem)
+    // loop through the todolist array and create new list items
+    for (const item of todoList) {
+        // create list item
+        const listItem = document.createElement('li')
+        listItem.textContent = item.text
+
+        // add item to list
+        todoListElement.appendChild(listItem)
+
+    }
+
+
+
+
 
 }
